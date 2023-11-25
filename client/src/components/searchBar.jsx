8080b831a75d9d0f1cb751c131701d98bb95ae46
Tiffany Perez <pereztb02@gmail.com>
2023-11-25 14:28:34 -0500

    worked on carousel and search bar

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -1,31 +1,16 @@
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Dropdown from "react-bootstrap/Dropdown";
+import './searchbar.css';
 
 function SearchBar() {
   return (
     <div className="searchbar">
-        <style type="text/css">
-            {`
-            .btn-success {
-                background-color: blue;
-                justify-content-end;
-                width: "90%";
-
-            }
-            .btn-success:hover {
-                background-color: #0000ff8f;
-            }
-            .btn-success:click {
-                background-color: #0000ff8f;
-            }
-            `}
-        </style>
       <Dropdown as={ButtonGroup}>
-        <Button variant="success">
+        <Button>
           <input type="search" placeholder="Search here" />
         </Button>
-        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+        <Dropdown.Toggle split id="dropdown-split-basic" />
 
         <Dropdown.Menu>
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,10 +1,12 @@
 import SearchBar from "../components/searchBar";
 import Slideshow from "../components/slideShow";
+import CartBtn from "../components/cart";
 
 export default function HomePage() {
   return (
     <div>
       <SearchBar />
+      <CartBtn />
       <Slideshow />
     </div>
   );
