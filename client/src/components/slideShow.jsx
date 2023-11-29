b61a72a4277a05b1ac8dc533c9d59cb40bd8d49a
HadiqaAziz <hadiqa.aziz@gmail.com>
2023-11-29 13:49:10 -0800

    fixed the slideshow

--- client/src/components/slideShow.jsx
+++ client/src/components/slideShow.jsx
@@ -2,7 +2,7 @@ import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 import jewelryImage from "../assets/jewelery_stock.jpg";
 import lawnMowerImage from "../assets/lawnmower_stock.jpg";
-import tennisRacketImage from "../assets/tenni_racket_stock.jpg";
+import tennisRacketImage from "../assets/wall img.jpg";
 import './slideShow.css';
 
 function Slideshow() {
@@ -11,23 +11,23 @@ function Slideshow() {
       <Carousel.Item className="slide">
         <Image className="image" src={jewelryImage} fluid />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h3>Welcome to TECH Cart</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Image className="image" src={lawnMowerImage} fluid />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h3>Welcome to TECH Cart</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Image className="image" src={tennisRacketImage} fluid />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Welcome to TECH Cart</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Empowering Your Clicks, Elevating Your Shopping Experience – Where Tech Meets Trend!
           </p>
         </Carousel.Caption>
       </Carousel.Item>
